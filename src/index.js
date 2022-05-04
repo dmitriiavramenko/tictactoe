ffdbff7f3f8c59635438d1de6eb7f44f01a84394
Dmitrii <feltocheese@gmail.com>
2022-05-04 18:09:13 -0400

    Square as a function component

--- src/index.js
+++ src/index.js
@@ -2,89 +2,81 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={() => {
-                this.props.onClick();
-                console.log("Button #" + this.props.value +" clicked!");
-        }}>
-          {this.props.value}
-        </button>
-      );
+function Square(props) {
+  return (
+    <button
+        className='square'
+        onClick={props.onClick}
+        >
+        {props.value}
+    </button>
+  );
+}
+  
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null) // To create array inside of a state
     }
-
-    
   }
-  
-  class Board extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null) // To create array inside of a state
-      }
-    }
+  handleClick(i) {
+    const squares = this.state.squares.slice(); // Makes copy of the array
+    squares[i] = 'X';                           // Immutability is important!
+    this.setState({squares: squares});
+  }
+  renderSquare(i) {
+    return (
+      <Square // Uses state as props inside of daughter components and passes onClick
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} // Reacts asks to provide "on" for event name
+      />                                    // and "handle" for method name
+    );
+  }
 
-    handleClick(i) {
-      const squares = this.state.squares.slice(); // Makes copy of the array
-      squares[i] = 'X';
-      this.setState({squares: squares});
-    }
+ render() {
+   const status = 'Next player: X';
 
-    renderSquare(i) {
-      return (
-        <Square // Uses state as props inside of daughter components and passes onClick
-          value={this.state.squares[i]}
-          onClick={() => this.handleClick(i)} // Reacts asks to provide "on" for event name
-        />                                    // and "handle" for method name
-      );
-    }
-  
-    render() {
-      const status = 'Next player: X';
-  
-      return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+   return (
+     <div>
+       <div className="status">{status}</div>
+       <div className="board-row">
+         {this.renderSquare(0)}
+         {this.renderSquare(1)}
+         {this.renderSquare(2)}
+       </div>
+       <div className="board-row">
+         {this.renderSquare(3)}
+         {this.renderSquare(4)}
+         {this.renderSquare(5)}
+       </div>
+       <div className="board-row">
+         {this.renderSquare(6)}
+         {this.renderSquare(7)}
+         {this.renderSquare(8)}
+       </div>
+     </div>
+   );
+ }
+}
+ 
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
         </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
   
   // ========================================
   
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
